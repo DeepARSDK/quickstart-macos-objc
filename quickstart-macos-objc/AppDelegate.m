7c6a9ac28b2b08ec4954f6a8e25fc4e86a40a1c6
Dorian Brmbota <dorian.brmbota@outlook.com>
2020-08-19 09:57:12 +0200

    Added example macos project

---
+++ quickstart-macos-objc/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  deepar-macos-example
+//
+//  Created by Kod Biro on 07/08/2020.
+//  Copyright © 2020 Kod Biro. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)application {
+  return YES;
+}
+
+
+@end
---
+++ quickstart-macos-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  deepar-macos-example
+//
+//  Created by Kod Biro on 07/08/2020.
+//  Copyright © 2020 Kod Biro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
